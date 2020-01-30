307ce739928538a56aa5c6b2466e2a4dfad6f9d6
Ig101 <nsn1001@mail.ru>
2020-01-31 00:45:56 +0300

    Fix cry on wait

--- BluePlague.WebClient/src/app/engine/tag-actions/cry-on-wait.actions.ts
+++ BluePlague.WebClient/src/app/engine/tag-actions/cry-on-wait.actions.ts
@@ -4,12 +4,13 @@ import { ReactionResult } from '../scene/models/reaction-result.model';
 import { ActionTag } from '../scene/models/action-tag.model';
 import { ReactionMessageLevelEnum } from '../models/enums/reaction-message-level.enum';
 
-function cryOnWaitWaitOutgoing(scene: Scene, object: Actor, x: number, y: number,
-                               weight?: number, strength?: number): ReactionResult  {
-  return {
+function cryOnWaitWaitOutgoing(scene: Scene, object: Actor,
+                               weight?: number, strength?: number)  {
+  const reaction = {
     level: ReactionMessageLevelEnum.Attention,
     message: [object.name, 'cry-on-wait']
   };
+  object.doReactiveAction('shout', reaction, [], 0);
 }
 
 export function registerCryOnWaitTag(): ActionTag<Actor> {
