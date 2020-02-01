dd3322a70aa7988da2f871ba35f94e0333344774
Ig101 <nsn1001@mail.ru>
2020-02-01 17:15:47 +0300

    Add interval

--- BluePlague.WebClient/src/app/game/ascii-game/ascii-game.component.ts
+++ BluePlague.WebClient/src/app/game/ascii-game/ascii-game.component.ts
@@ -14,21 +14,38 @@ export class AsciiGameComponent implements OnInit, OnDestroy {
 
   @ViewChild('gameCanvas', { static: true }) gameCanvas: ElementRef<HTMLCanvasElement>;
 
-  canvasWidth = 1120;
-  canvasHeight = 630;
+  drawingTimer;
+  changed = false;
 
-  private tileWidth;
-  private readonly tileHeight = 40;
-  private readonly defaultManyActorsSprite = {
+  tileWidth = 0;
+  tileHeight = 40;
+  defaultManyActorsSprite = {
     character: '*',
     color: {r: 255, g: 255, b: 0, a: 1}
   } as SpriteSnapshot;
 
-  private canvasContext: CanvasRenderingContext2D;
+  canvasWidthInternal = 1120;
+  canvasHeightInternal = 630;
 
-  get sceneString(): string {
-    return this.gameStateService.scene ? JSON.stringify(this.gameStateService.scene) : 'no scene';
+  get canvasWidth() {
+    return this.canvasWidthInternal;
   }
+
+  set canvasWidth(value: number) {
+    this.changed = true;
+    this.canvasWidthInternal = value;
+  }
+
+  get canvasHeight() {
+    return this.canvasHeightInternal;
+  }
+
+  set canvasHeight(value: number) {
+    this.changed = true;
+    this.canvasHeightInternal = value;
+  }
+
+  private canvasContext: CanvasRenderingContext2D;
 
   constructor(
     private gameStateService: GameStateService,
@@ -45,17 +62,18 @@ export class AsciiGameComponent implements OnInit, OnDestroy {
         this.gameStateService.cameraX = result.player.x;
         this.gameStateService.cameraY = result.player.y;
         this.engineFacadeService.subscribeOnActionsResult(this.processNewAction, this.sceneWasDeleted);
-        this.redrawScene();
+        this.drawingTimer = setInterval(this.redrawScene, 16, this);
       });
   }
 
   ngOnDestroy(): void {
-
+    clearInterval(this.drawingTimer);
   }
 
 
   processNewAction(response: EngineActionResponse) {
-
+    // TODO process changes
+    this.changed = true;
   }
 
   sceneWasDeleted() {
@@ -63,58 +81,59 @@ export class AsciiGameComponent implements OnInit, OnDestroy {
   }
 
   // Drawing
-  private drawPoint(scene: SceneSnapshot, x: number, y: number, cameraLeft: number, cameraTop: number) {
+  private drawPoint(context: AsciiGameComponent, scene: SceneSnapshot, x: number, y: number, cameraLeft: number, cameraTop: number) {
     const tile = scene.tiles[x][y];
-    const canvasX = (x - cameraLeft) * this.tileWidth;
-    const canvasY = (y - cameraTop) * this.tileHeight;
-    const symbolY = canvasY + this.tileHeight * 0.7;
-    this.canvasContext.fillStyle = `rgb(${tile.backgroundColor.r}, ${tile.backgroundColor.g}, ${tile.backgroundColor.b})`;
-    this.canvasContext.fillRect(canvasX, canvasY, this.tileWidth, this.tileHeight);
+    const canvasX = (x - cameraLeft) * context.tileWidth;
+    const canvasY = (y - cameraTop) * context.tileHeight;
+    const symbolY = canvasY + context.tileHeight * 0.7;
+    context.canvasContext.fillStyle = `rgb(${tile.backgroundColor.r}, ${tile.backgroundColor.g}, ${tile.backgroundColor.b})`;
+    context.canvasContext.fillRect(canvasX, canvasY, context.tileWidth, context.tileHeight);
     if (tile.objects.length > 0) {
       const mainObject = tile.objects.find(object => !object.passable);
       if (mainObject) {
-        this.canvasContext.fillStyle = `rgba(${mainObject.sprite.color.r}, ${mainObject.sprite.color.g},
+        context.canvasContext.fillStyle = `rgba(${mainObject.sprite.color.r}, ${mainObject.sprite.color.g},
           ${mainObject.sprite.color.b}, ${mainObject.sprite.color.a})`;
-        this.canvasContext.fillText(mainObject.sprite.character, canvasX, symbolY);
+        context.canvasContext.fillText(mainObject.sprite.character, canvasX, symbolY);
         return;
       }
       if (tile.objects.length > 1) {
-        this.canvasContext.fillStyle = `rgba(${this.defaultManyActorsSprite.color.r}, ${this.defaultManyActorsSprite.color.g},
-          ${this.defaultManyActorsSprite.color.b}, ${this.defaultManyActorsSprite.color.a})`;
-        this.canvasContext.fillText(this.defaultManyActorsSprite.character, canvasX, symbolY);
+        context.canvasContext.fillStyle = `rgba(${context.defaultManyActorsSprite.color.r},
+          ${context.defaultManyActorsSprite.color.g},
+          ${context.defaultManyActorsSprite.color.b},
+          ${context.defaultManyActorsSprite.color.a})`;
+        context.canvasContext.fillText(context.defaultManyActorsSprite.character, canvasX, symbolY);
         return;
       }
       const firstObject = tile.objects[0];
-      this.canvasContext.fillStyle = `rgba(${firstObject.sprite.color.r}, ${firstObject.sprite.color.g},
+      context.canvasContext.fillStyle = `rgba(${firstObject.sprite.color.r}, ${firstObject.sprite.color.g},
         ${firstObject.sprite.color.b}, ${firstObject.sprite.color.a})`;
-      this.canvasContext.fillText(firstObject.sprite.character, canvasX, symbolY);
+      context.canvasContext.fillText(firstObject.sprite.character, canvasX, symbolY);
       return;
     }
-    this.canvasContext.fillStyle = `rgba(${tile.sprite.color.r}, ${tile.sprite.color.g}, ${tile.sprite.color.b}, ${tile.sprite.color.a})`;
-    this.canvasContext.fillText(tile.sprite.character, canvasX, symbolY);
+    context.canvasContext.fillStyle = `rgba(${tile.sprite.color.r}, ${tile.sprite.color.g},
+      ${tile.sprite.color.b}, ${tile.sprite.color.a})`;
+    context.canvasContext.fillText(tile.sprite.character, canvasX, symbolY);
   }
 
-  private redrawScene() {
-    const snapshot = this.gameStateService.scene;
+  private redrawScene(context: AsciiGameComponent) {
+    if (!context.gameStateService.changed && !context.changed) {
+      return;
+    }
+    context.gameStateService.changed = false;
+    context.changed = false;
+    const snapshot = context.gameStateService.scene;
     if (snapshot) {
-      console.log('startWrite');
-      const cameraLeft = this.gameStateService.cameraX - this.canvasWidth / 2 / this.tileWidth + 0.5;
-      const cameraTop = this.gameStateService.cameraY - this.canvasHeight / 2 / this.tileHeight + 0.5;
-      this.canvasContext.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
-      this.canvasContext.font = `${this.tileHeight * 1}px PT Mono`;
+      const cameraLeft = context.gameStateService.cameraX - context.canvasWidth / 2 / context.tileWidth + 0.5;
+      const cameraTop = context.gameStateService.cameraY - context.canvasHeight / 2 / context.tileHeight + 0.5;
+      context.canvasContext.clearRect(0, 0, context.canvasWidth, context.canvasHeight);
+      context.canvasContext.font = `${context.tileHeight * 1}px PT Mono`;
       const left = Math.max(0, Math.floor(cameraLeft));
-      const right = Math.min(snapshot.width - 1, Math.ceil(cameraLeft + this.canvasWidth / this.tileWidth));
+      const right = Math.min(snapshot.width - 1, Math.ceil(cameraLeft + context.canvasWidth / context.tileWidth));
       const top = Math.max(0, Math.floor(cameraTop));
-      const bottom = Math.min(snapshot.height - 1, Math.ceil(cameraTop + this.canvasHeight / this.tileHeight));
-      console.log(left);
-      console.log(right);
-      console.log(top);
-      console.log(bottom);
-      console.log(cameraLeft);
-      console.log(cameraTop);
+      const bottom = Math.min(snapshot.height - 1, Math.ceil(cameraTop + context.canvasHeight / context.tileHeight));
       for (let x = left; x <= right; x++) {
         for (let y = top; y <= bottom; y++ ) {
-          this.drawPoint(snapshot, x, y, cameraLeft, cameraTop);
+          context.drawPoint(context, snapshot, x, y, cameraLeft, cameraTop);
         }
       }
     }
--- BluePlague.WebClient/src/app/game/services/game-state.service.ts
+++ BluePlague.WebClient/src/app/game/services/game-state.service.ts
@@ -4,13 +4,41 @@ import { SceneSnapshot } from 'src/app/engine/models/scene/scene-snapshot.model'
 @Injectable()
 export class GameStateService {
 
-  scene: SceneSnapshot;
-
-  cameraX = 0;
-  cameraY = 0;
+  changed = false;
 
   defaultWidth = 1920;
   defaultHeight = 1080;
+
+  private sceneInternal: SceneSnapshot;
+  private cameraXInternal = 0;
+  private cameraYInternal = 0;
+
+  get scene() {
+    return this.sceneInternal;
+  }
+
+  set scene(value: SceneSnapshot) {
+    this.changed = true;
+    this.sceneInternal = value;
+  }
+
+  get cameraX() {
+    return this.cameraXInternal;
+  }
+
+  set cameraX(value: number) {
+    this.changed = true;
+    this.cameraXInternal = value;
+  }
+
+  get cameraY() {
+    return this.cameraYInternal;
+  }
+
+  set cameraY(value: number) {
+    this.changed = true;
+    this.cameraYInternal = value;
+  }
 
   constructor() { }
 }
