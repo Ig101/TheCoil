962f3147cf5054fe546d1db768ae4e4131511933
Ig101 <nsn1001@mail.ru>
2020-02-10 21:06:06 +0300

    Add random

--- BluePlague.WebClient/src/app/shared/services/random.service.ts
+++ BluePlague.WebClient/src/app/shared/services/random.service.ts
@@ -1,39 +1,99 @@
 import { Injectable } from '@angular/core';
 import { WebCommunicationService } from './web-communication.service';
 
+// Algorithm from .NET for compatibility with backend (c) .NET Foundation and Contributors
 @Injectable()
 export class RandomService {
 
-  private seed = 0;
-  private sheet = 0;
+  private seed: number;
 
-  readonly maxValue = 1000000;
+  private inext: number;
+  private inextp: number;
+  private seedArray: number[] = new Array<number>(56);
 
-  constructor(private webCommunicationsService: WebCommunicationService) { }
+  private readonly maxValue = 2147483647;
+  private readonly maxSeed = 161803398;
+
+  constructor() {
+    this.setupNewSeed(0);
+  }
 
   setupNewSeed(seed: number) {
+    let ii: number;
+    let mj: number;
+    let mk: number;
+
+    const subtraction = Math.abs(seed);
+    mj = this.maxSeed - subtraction;
+    this.seedArray[55] = mj;
+    mk = 1;
+    for (let i = 1; i < 55; i++) {
+      ii = (21 * i) % 55;
+      this.seedArray[ii] = mk;
+      mk = mj - mk;
+      if (mk < 0) {
+        mk += this.maxValue;
+      }
+      mj = this.seedArray[ii];
+    }
+    for (let k = 1; k < 5; k++) {
+      for (let i = 1; i < 56; i++) {
+        this.seedArray[i] -= this.seedArray[1 + (i + 30) % 55];
+        if (this.seedArray[i] < 0) {
+          this.seedArray[i] += this.maxValue;
+        }
+      }
+    }
+    this.inext = 0;
+    this.inextp = 21;
     this.seed = seed;
-    this.sheet = 0;
   }
 
   reset() {
     this.setupNewSeed(this.seed);
   }
 
+  private sample() {
+    let retVal;
+    let locINext = this.inext;
+    let locINextp = this.inextp;
+
+    if (++locINext >= 56) {
+      locINext = 1;
+    }
+    if (++locINextp >= 56) {
+      locINextp = 1;
+    }
+
+    retVal = this.seedArray[locINext] - this.seedArray[locINextp];
+
+    if (retVal === this.maxValue) {
+      retVal--;
+    }
+    if (retVal < 0) {
+      retVal += this.maxValue;
+    }
+
+    this.seedArray[locINext] = retVal;
+
+    this.inext = locINext;
+    this.inextp = locINextp;
+    return retVal;
+}
+
   // from -max to max
-  next(fromSeed: boolean = false, start: number = -this.maxValue, end: number = this.maxValue): number {
+  next(start: number = -this.maxValue, end: number = this.maxValue): number {
     if (end > this.maxValue) {
       end = this.maxValue;
     }
     if (start < -this.maxValue) {
       start = -this.maxValue;
     }
-    return Math.floor(this.nextDouble(fromSeed) * (end - start) + start);
+    return Math.floor(this.nextDouble() * (end - start) + start);
   }
 
   // from 0 to 1
-  nextDouble(fromSeed: boolean = false): number {
-    // TODO get from sheets
-    return Math.random();
+  nextDouble(): number {
+    return this.sample() * (1.0 / this.maxValue);
   }
 }
